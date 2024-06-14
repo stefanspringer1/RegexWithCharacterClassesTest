adca765eb4bcce22dee54f73318ec4cebc858adb
stefanspringer1 <stefan.springer@icloud.com>
2024-06-14 16:22:33 +0200

    added comment

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,5 @@
 import RegexWithCharacterClasses
 
+// note that the type annotation is not necessary:
 let regex: Regex<Substring> = #regexWithCharacterClasses("[${LATIN_LETTERS}]")
 print("123 hello!".replacing(regex, with: "x")) // "123 xxxxx!"
