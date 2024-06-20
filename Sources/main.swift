ea4503cec87600498fd12f0a2893ad46e6c62dff
stefanspringer1 <stefan.springer@icloud.com>
2024-06-20 15:49:50 +0200

    using RegexWithCharacterClasses 0.0.14

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,12 +2,12 @@ import RegexWithCharacterClasses
 
 do {
     // note that the type annotation is not necessary:
-    let regex: any RegexComponent = #regexWithCharacterClasses("[${LATIN_LETTERS}]")
+    let regex: any RegexComponent = #regexWithCharacterClassesAnd1Group("[${LATIN_LETTERS}]")
     print("123 hello!".replacing(regex, with: "x")) // "123 xxxxx!"
 }
 
 do {
     // note that the type annotation is not necessary:
-    let regex: any RegexComponent = #regexWithCharacterClasses("[${COMBINING}]", matchingSemantics: .unicodeScalar)
+    let regex: any RegexComponent = #regexWithCharacterClassesAnd1Group("[${COMBINING}]").matchingSemantics(.unicodeScalar)
     print("a\u{0307}".replacing(regex, with: "x")) // "ax"
 }
