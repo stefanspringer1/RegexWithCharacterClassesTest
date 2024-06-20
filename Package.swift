6dcc41633de688d28198485592d22a93af4db863
stefanspringer1 <stefan.springer@icloud.com>
2024-06-20 11:38:05 +0200

    using RegexWithCharacterClasses 0.0.8

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             targets: ["RegexWithCharacterClassesTest"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/stefanspringer1/RegexWithCharacterClasses.git", from: "0.0.7"),
+        .package(url: "https://github.com/stefanspringer1/RegexWithCharacterClasses.git", from: "0.0.8"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
